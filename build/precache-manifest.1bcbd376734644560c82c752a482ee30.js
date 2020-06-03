1371ef161b408a48962b4a3461ad56eec9adaa01
unknown <ananddevsingh.aiet@gmail.com>
2020-06-03 15:03:01 +0530

    Pusing build folder

---
+++ build/precache-manifest.1bcbd376734644560c82c752a482ee30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd10c51411d9bd2972b43798c878b6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e253e5f48db7c46040b9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77f2e1146858d1aa1a06",
+    "url": "/static/js/2.4450f130.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4450f130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e253e5f48db7c46040b9",
+    "url": "/static/js/main.8d4aa555.chunk.js"
+  },
+  {
+    "revision": "458226ec4fa5afc92205",
+    "url": "/static/js/runtime-main.063172f8.js"
+  }
+]);
